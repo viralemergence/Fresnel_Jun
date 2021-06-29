1dab1010cba6d1e5ff5f9bce89bfa094eb3144be
gfalbery <gfalbery@gmail.com>
2021-06-29 13:44:13 +0100

    Pretty sure that all worked

--- R/01_Fresnel Import.R
+++ R/01_Fresnel Import.R
@@ -13,46 +13,6 @@ rm(list = ls())
 
 here::here() %>% setwd()
 
-# Dictating whether to add the most up-to-date betacov predictions ####
-
-AddNewData <- T
-
-if(AddNewData){
-  
-  if(file.exists(paste0(here::here(), 
-                        '/Github/Repos/virionette/03_interaction_data/OldVirionette.csv'))){
-    
-    Virionette <- read_csv(paste0(here::here(), 
-                                  '/Github/Repos/virionette/03_interaction_data/OldVirionette.csv'))
-    
-  }else{
-    
-    Virionette <- read_csv(paste0(here::here(), 
-                                  '/Github/Repos/virionette/03_interaction_data/virionette.csv'))
-    
-    Virionette %>% 
-      write.csv(paste0(here::here(), 
-                       '/Github/Repos/virionette/03_interaction_data/OldVirionette.csv'))
-    
-  }
-  
-  BinaryWebsite <- read_csv("BinaryWebsiteNew.csv")
-  
-  NewData <- 
-    BinaryWebsite %>% filter(`New data` == "New data") %>% 
-    mutate(virus_genus = "Betacoronavirus", host_order = "Chiroptera") %>% 
-    dplyr::select(host_species = Sp, host_order, virus_genus)
-  
-  Virionette %>% 
-    # anti_join(NewData, by = c("host_species", "host_order", "virus_genus")) %>% #nrow
-    bind_rows(NewData) %>% 
-    # nrow
-    mutate_at("host_species", ~str_replace_all(.x, "Myonycteris angolensis", "Lissonycteris angolensis")) %>% 
-    write.csv(paste0(here::here(), 
-                     '/Github/Repos/virionette/03_interaction_data/virionette.csv'))
-  
-}
-
 Virionette <- 
   read_csv(paste0(here::here(),
                   "/GitHub/Repos/virionette/03_interaction_data/virionette.csv"))
@@ -85,9 +45,9 @@ farrell1 <- read_csv(paste0(GithubDir, "FarrellBatFull.csv"))
 farrell2 <- read_csv(paste0(GithubDir, "FarrellBatPhylogeny.csv"))
 guth1 <- read_csv(paste0(GithubDir, "GuthCitations.csv"))
 guth2 <- read_csv(paste0(GithubDir, "GuthUncorrected.csv"))
-poisot1 <- read_csv(paste0(GithubDir, "PoisotKnn1Bat.csv"), col_names = FALSE)
-poisot2 <- read_csv(paste0(GithubDir, "PoisotKnn2Bat.csv"), col_names = FALSE)
-poisot3 <- read_csv(paste0(GithubDir, "PoisotLfBat.csv"), col_names = FALSE)
+poisot1 <- read_csv(paste0(GithubDir, "PoisotKnn1Bat-June.csv"), col_names = FALSE)
+poisot2 <- read_csv(paste0(GithubDir, "PoisotKnn2Bat-June.csv"), col_names = FALSE)
+poisot3 <- read_csv(paste0(GithubDir, "PoisotLfBat-June.csv"), col_names = FALSE)
 stock1 <- read_csv(paste0(GithubDir, 
                           "Stock_both.csv"), 
                    col_names = TRUE)
@@ -138,7 +98,7 @@ ModelList %>%
         mutate_at(vars(starts_with("P.")), function(a) max(na.omit(a)) - a + 1) %>%
         rename_all(function(a) str_replace_all(a, "^P.", "R.")) %>%
         bind_cols(.x %>% dplyr::select(starts_with("P.")))) %>%
-  reduce(full_join) %>% 
+  reduce(~full_join(.x, .y, by = "Sp")) %>% 
   mutate_at("Sp", ~.x %>% 
               str_trim %>% 
               str_replace("_", " ")) -> 
@@ -213,9 +173,9 @@ dallas1 <- read_csv(paste0(GithubDir, "DallasMammalsCitations.csv"))
 dallas2 <- read_csv(paste0(GithubDir, "DallasMammalsUncorrected.csv"))
 farrell1 <- read_csv(paste0(GithubDir, "FarrellMammalsFull.csv"))
 farrell2 <- read_csv(paste0(GithubDir, "FarrellMammalsPhylogeny.csv"))
-poisot1 <- read_csv(paste0(GithubDir, "PoisotKnn1Mammal.csv"), col_names = FALSE)
-poisot2 <- read_csv(paste0(GithubDir, "PoisotKnn2Mammal.csv"), col_names = FALSE)
-poisot3 <- read_csv(paste0(GithubDir, "PoisotLfMammal.csv"), col_names = FALSE)
+poisot1 <- read_csv(paste0(GithubDir, "PoisotKnn1Mammal-June.csv"), col_names = FALSE)
+poisot2 <- read_csv(paste0(GithubDir, "PoisotKnn2Mammal-June.csv"), col_names = FALSE)
+poisot3 <- read_csv(paste0(GithubDir, "PoisotLfMammal-June.csv"), col_names = FALSE)
 
 # Triaging column names ####
 
@@ -244,7 +204,7 @@ ModelList %>%
         mutate_at(vars(starts_with("P.")), function(a) max(na.omit(a)) - a + 1) %>%
         rename_all(function(a) str_replace_all(a, "^P.", "R.")) %>%
         bind_cols(.x %>% select(starts_with("P.")))) %>%
-  reduce(full_join) %>% 
+  reduce(~full_join(.x, .y, by = "Sp")) %>% 
   mutate_at("Sp", ~.x %>% 
               str_trim %>% 
               str_replace("_", " ")) %>% 
@@ -314,7 +274,6 @@ NonBatModels_OS %<>% select(Sp, Betacov, R.Alb, Rank, PropRank, InSample)
 NonBatModels_OS %<>% arrange(PropRank)
 NonBatModels_IS %<>% arrange(PropRank)
 
-
 dir_create("Cleaned Files")
 
 BatModels_OS %>% write.csv("Cleaned Files/BatModels_OS.csv", row.names = F)
@@ -322,52 +281,290 @@ BatModels_IS %>% write.csv("Cleaned Files/BatModels_IS.csv", row.names = F)
 NonBatModels_OS %>% write.csv("Cleaned Files/NonBatModels_OS.csv", row.names = F)
 NonBatModels_IS %>% write.csv("Cleaned Files/NonBatModels_IS.csv", row.names = F)
 
+# ~~~~~ Same but for 2020 ####
 
-if(0){
+# Importing predictions and creating ranked predictions ####
+
+library(tidyverse); library(fs); library(magrittr); library(ggregplot)
+library(conflicted); library(dplyr)
+
+conflict_prefer("map", "purrr")
+
+c("select", "filter", "intersect", "summarise", "mutate", "rename", "arrange") %>%
+  map(~conflict_prefer(.x, "dplyr"))
+
+rm(list = ls())
+
+here::here() %>% setwd()
+
+Virionette <- 
+  read_csv(paste0(here::here(),
+                  "/GitHub/Repos/virionette/03_interaction_data/Oldvirionette.csv"))
+
+# BatsVsOther <- read.csv("Data/BatsVsOther.csv") %>%
+#   rename_all(CamelConvert) %>% 
+#   mutate_at("Bats", ~.x %>% as.character %>% CamelConvert)
+
+Repos <- c(
+  "albery-betacov",
+  "becker-betacov",
+  "carlson-batcov",
+  "dallas-betacov",
+  "farrell-betacov",
+  "guth-betacov",
+  "poisot-betacov",
+  "stock-betacov")
+
+# ~~~~~ Bats ####
+
+GithubDir <- "Github/CSVs_2020/"
+
+albery <- read_csv(paste0(GithubDir, "AlberyBats.csv"))
+carlson1 <- read_csv(paste0(GithubDir, "CarlsonBartCitations.csv"))
+carlson2 <- read_csv(paste0(GithubDir, "CarlsonBartUncorrected.csv"))
+carlson3 <- read_csv(paste0(GithubDir, "CarlsonDartCitations.csv"))
+carlson4 <- read_csv(paste0(GithubDir, "CarlsonDartUncorrected.csv"))
+dallas1 <- read_csv(paste0(GithubDir,"DallasMammalsCitations.csv"))
+farrell1 <- read_csv(paste0(GithubDir, "FarrellBatFull.csv"))
+farrell2 <- read_csv(paste0(GithubDir, "FarrellBatPhylogeny.csv"))
+guth1 <- read_csv(paste0(GithubDir, "GuthCitations.csv"))
+guth2 <- read_csv(paste0(GithubDir, "GuthUncorrected.csv"))
+poisot1 <- read_csv(paste0(GithubDir, "PoisotKnn1Bat.csv"), col_names = FALSE)
+poisot2 <- read_csv(paste0(GithubDir, "PoisotKnn2Bat.csv"), col_names = FALSE)
+poisot3 <- read_csv(paste0(GithubDir, "PoisotLfBat.csv"), col_names = FALSE)
+stock1 <- read_csv(paste0(GithubDir, 
+                          "Stock_both.csv"), 
+                   col_names = TRUE)
+
+# Triaging column names ####
+
+albery %>% rename(P.Alb = Count) -> albery
+carlson1 %>% select(host_species, pred) %>% rename(Sp = host_species, P.Car1 = pred) -> carlson1
+carlson2 %>% select(host_species, pred) %>% rename(Sp = host_species, P.Car2 = pred) -> carlson2
+carlson3 %>% select(host_species, pred) %>% rename(Sp = host_species, P.Car3 = pred) -> carlson3
+carlson4 %>% select(host_species, pred) %>% rename(Sp = host_species, P.Car4 = pred) -> carlson4
+dallas1 %>% select(host, suitability) %>% rename(Sp = host, P.Dal1 = suitability) -> dallas1
+farrell1 %>% select(Host, p.interaction) %>% rename(Sp = Host, P.Far1 = p.interaction) -> farrell1
+farrell2 %>% select(Host, p.interaction) %>% rename(Sp = Host, P.Far2 = p.interaction) -> farrell2
+guth1 %>% select(host_species, pred_med) %>% rename(Sp = host_species, P.Gut1 = pred_med) -> guth1
+guth2 %>% select(host_species, pred_med) %>% rename(Sp = host_species, P.Gut2 = pred_med) -> guth2
+poisot1 %>% rename(Sp = X1, P.Po1 = X2) -> poisot1
+poisot2 %>% rename(Sp = X1, P.Po2 = X2) -> poisot2
+poisot3 %>% rename(Sp = X1, P.Po3 = X2) -> poisot3
+
+stock1 %>% select(Sp = X1, P.Stock1 = Betacoronavirus) %>% 
+  arrange(desc(P.Stock1)) ->
   
-  batin %>% gather("Key", "Value", -c(Sp, Betacov, Rank, PropRank, InSample)) %>%
-    left_join(
-      
-      batin2 %>% gather("Key", "Value", -c(Sp, Betacov, Rank, PropRank, InSample)),
-      
-      by = c("Sp", "Key")
-    ) %>% 
-    
-    ggplot(aes(Value.x, Value.y)) + geom_point() + 
-    facet_wrap(~Key)
+  StockDF
+
+# Specifically subset Tad ####
+
+assoc <- Virionette %>% 
+  filter(host_order == 'Chiroptera')
+
+dallas1 %>% as_tibble %>% 
+  mutate(InAssocBats = (Sp %in% assoc$host_species)) %>%
+  filter(InAssocBats == 1) -> dallas1
+
+# Drops or not ####
+
+ModelList <- list(albery, carlson3, dallas1, farrell1, 
+                  guth1, poisot2, poisot3, StockDF) 
+
+# Mutating ####
+
+ModelList %>%
+  map(~.x %>% mutate_at(vars(contains("Po")), ~str_remove(.x, "X") %>% as.numeric) %>% 
+        mutate_at("Sp", ~str_replace_all(.x, "[.]", " "))) %>% 
+  map(~.x %>% 
+        mutate_at("Sp", function(a) a %>% 
+                    str_trim %>% 
+                    str_replace("_", " ")) %>%
+        mutate_at(vars(starts_with("P.")), function(a) rank(a, na.last = "keep")) %>%
+        mutate_at(vars(starts_with("P.")), function(a) max(na.omit(a)) - a + 1) %>%
+        rename_all(function(a) str_replace_all(a, "^P.", "R.")) %>%
+        bind_cols(.x %>% dplyr::select(starts_with("P.")))) %>%
+  reduce(~full_join(.x, .y, by = "Sp")) %>% 
+  mutate_at("Sp", ~.x %>% 
+              str_trim %>% 
+              str_replace("_", " ")) -> 
   
-  batout %>% gather("Key", "Value", -c(Sp, Betacov, Rank, PropRank, InSample)) %>%
-    left_join(
-      
-      batout2 %>% gather("Key", "Value", -c(Sp, Betacov, Rank, PropRank, InSample)),
-      
-      by = c("Sp", "Key")
-    ) %>% 
-    
-    ggplot(aes(Value.x, Value.y)) + geom_point() + 
-    facet_wrap(~Key)
+  Models
+
+# Add in betacov true/false
+
+Virionette %>% 
   
-  mamin %>% gather("Key", "Value", -c(Sp, Betacov, Rank, PropRank, InSample)) %>%
-    left_join(
-      
-      mamin2 %>% gather("Key", "Value", -c(Sp, Betacov, Rank, PropRank, InSample)),
-      
-      by = c("Sp", "Key")
-    ) %>% 
-    
-    ggplot(aes(Value.x, Value.y)) + geom_point() + 
-    facet_wrap(~Key)
+  group_by(host_species) %>% 
+  summarise(Betacov = as.numeric(any(virus_genus == "Betacoronavirus"))) %>%
   
-  mamout %>% gather("Key", "Value", -c(Sp, Betacov, Rank, PropRank, InSample)) %>%
-    left_join(
-      
-      mamout2 %>% gather("Key", "Value", -c(Sp, Betacov, Rank, PropRank, InSample)),
-      
-      by = c("Sp", "Key")
-    ) %>% 
-    
-    ggplot(aes(Value.x, Value.y)) + geom_point() + 
-    facet_wrap(~Key)
+  select(Sp = host_species, Betacov) %>%
+  mutate_at("Sp", ~.x %>% str_trim %>% str_replace_all("_"," ")) -> 
+  truth
+
+Models %>% 
+  left_join(truth) %>% 
+  mutate(Betacov = replace_na(Betacov, 0)) ->
+  Models
+
+# Models %<>% select(-c(R.Stock2:R.Stock4))
+
+# Generate proportional rankings
+
+Models %<>% 
+  mutate(Rank = rowMeans(select(Models, starts_with("R.")), na.rm = TRUE))
+
+Models %>% select(starts_with("R.")) %>% names -> 
+  RankNames
+
+Models %<>% 
+  mutate_at(RankNames, ~.x/max(.x, na.rm = T)) %>%  
+  mutate(PropRank = rowMeans(select(Models, starts_with("R.")) %>% 
+                               mutate_at(RankNames, ~.x/max(.x, na.rm = T)), 
+                             na.rm = TRUE)) %>% 
+  arrange(Rank)
+
+Models %>% 
+  mutate_at("Sp", ~str_replace_all(.x, "Lissonycteris angolensis", 
+                                   "Myonycteris angolensis")) %>%
+  select(Sp, Betacov, starts_with("R."), starts_with("P."), 
+         Rank, PropRank) %>%
+  as.data.frame -> 
+  BatModels
+
+# Splitting in-sample 
+
+BatModels %<>% mutate(InSample = as.numeric(Sp %in% Virionette$host_species))
+
+BatModels_IS <- BatModels %>% filter(!(!InSample))
+BatModels_OS <- BatModels %>% filter(!(InSample))
+
+NACols <- BatModels_OS %>% is.na %>% colSums
+
+NACols[NACols >= (nrow(BatModels_OS) - nrow(BatModels_IS))] %>% 
+  names %>% setdiff(names(BatModels_OS), .) %>%
+  select(BatModels_OS, .) ->
   
-}
+  BatModels_OS
+
+BatModels_OS %<>% arrange(PropRank)
+BatModels_IS %<>% arrange(PropRank)
+
+# ~~~~~ NonBats ####
+
+GithubDir <- "Github/CSVs_2020/"
+
+albery <- read_csv(paste0(GithubDir, "AlberyNonBats.csv"))
+dallas1 <- read_csv(paste0(GithubDir, "DallasMammalsCitations.csv"))
+dallas2 <- read_csv(paste0(GithubDir, "DallasMammalsUncorrected.csv"))
+farrell1 <- read_csv(paste0(GithubDir, "FarrellMammalsFull.csv"))
+farrell2 <- read_csv(paste0(GithubDir, "FarrellMammalsPhylogeny.csv"))
+poisot1 <- read_csv(paste0(GithubDir, "PoisotKnn1Mammal.csv"), col_names = FALSE)
+poisot2 <- read_csv(paste0(GithubDir, "PoisotKnn2Mammal.csv"), col_names = FALSE)
+poisot3 <- read_csv(paste0(GithubDir, "PoisotLfMammal.csv"), col_names = FALSE)
+
+# Triaging column names ####
+
+albery %>% rename(P.Alb = Count) -> albery
+dallas1 %>% select(host, suitability) %>% rename(Sp = host, P.Dal1 = suitability) -> dallas1
+dallas2 %>% select(host, suitability) %>% rename(Sp = host, P.Dal2 = suitability) -> dallas2
+farrell1 %>% select(Host, p.interaction) %>% rename(Sp = Host, P.Far1 = p.interaction) -> farrell1
+farrell2 %>% select(Host, p.interaction) %>% rename(Sp = Host, P.Far2 = p.interaction) -> farrell2
+poisot1 %>% rename(Sp = X1, P.Po1 = X2) -> poisot1
+poisot2 %>% rename(Sp = X1, P.Po2 = X2) -> poisot2
+poisot3 %>% rename(Sp = X1, P.Po3 = X2) -> poisot3
+
+# Drops or not ####
+
+ModelList <- list(albery, dallas1, farrell1, poisot2, poisot3) 
+
+# Mutating ####
+
+ModelList %>%
+  map(~.x %>% 
+        mutate_at("Sp", function(a) a %>% 
+                    str_trim %>% 
+                    str_replace("_", " ")) %>%
+        #filter(!(Sp = 'Homo sapiens'))  %>% 
+        mutate_at(vars(starts_with("P.")), function(a) rank(a, na.last = "keep")) %>%
+        mutate_at(vars(starts_with("P.")), function(a) max(na.omit(a)) - a + 1) %>%
+        rename_all(function(a) str_replace_all(a, "^P.", "R.")) %>%
+        bind_cols(.x %>% select(starts_with("P.")))) %>%
+  reduce(~full_join(.x, .y, by = "Sp")) %>% 
+  mutate_at("Sp", ~.x %>% 
+              str_trim %>% 
+              str_replace("_", " ")) %>% 
+  select(Sp, starts_with("R.")) -> 
+  
+  Models
+
+# Add in betacov true/false
+
+Virionette %>% 
+  
+  group_by(host_species) %>% 
+  summarise(Betacov = as.numeric(any(virus_genus == "Betacoronavirus"))) %>%
+  
+  select(Sp = host_species, Betacov) %>%
+  mutate_at("Sp", ~.x %>% str_trim %>% str_replace_all("_"," ")) -> 
+  truth
+
+Models %>% 
+  left_join(truth) %>% 
+  mutate(Betacov = replace_na(Betacov, 0)) ->
+  Models
+
+# Models %<>% left_join(BatsVsOther %>%
+#                         mutate_at("Name", ~.x %>% 
+#                                     str_trim %>% 
+#                                     str_replace_all("_", " ")) %>%
+#                         dplyr::select(Name, Bats), 
+#                       by = c("Sp" = "Name")) %>%
+#   filter(Bats == "Other") %>%
+#   as.data.frame()
+
+# Generate proportional rankings
+
+Models %<>% 
+  mutate(Rank = rowMeans(select(Models, starts_with("R.")), na.rm = TRUE))
+
+Models %>% select(starts_with("R.")) %>% names -> 
+  RankNames
+
+Models %<>% 
+  mutate_at(RankNames, ~.x/max(.x, na.rm = T)) %>%  
+  mutate(PropRank = rowMeans(select(Models, starts_with("R.")) %>% 
+                               mutate_at(RankNames, ~.x/max(.x, na.rm = T)), 
+                             na.rm = TRUE)) %>% 
+  arrange(PropRank)
+
+Models %>% 
+  mutate_at("Sp", ~str_replace_all(.x, "Lissonycteris angolensis", 
+                                   "Myonycteris angolensis")) %>%
+  select(Sp, Betacov, starts_with("R."), starts_with("P."), 
+         Rank, PropRank) %>%
+  as.data.frame -> 
+  NonBatModels
+
+# Splitting in-sample 
+
+NonBatModels %<>% mutate(InSample = as.numeric(Sp %in% Virionette$host_species))
+
+NonBatModels_IS <- NonBatModels %>% filter(!(!InSample))
+NonBatModels_OS <- NonBatModels #%>% filter(!(InSample))
+
+NACols <- NonBatModels_OS %>% is.na %>% colSums
+
+NonBatModels_OS %<>% select(Sp, Betacov, R.Alb, Rank, PropRank, InSample)
+
+NonBatModels_OS %<>% arrange(PropRank)
+NonBatModels_IS %<>% arrange(PropRank)
+
+dir_create("Cleaned Files_2020")
+
+BatModels_OS %>% write.csv("Cleaned Files_2020/BatModels_OS.csv", row.names = F)
+BatModels_IS %>% write.csv("Cleaned Files_2020/BatModels_IS.csv", row.names = F)
+NonBatModels_OS %>% write.csv("Cleaned Files_2020/NonBatModels_OS.csv", row.names = F)
+NonBatModels_IS %>% write.csv("Cleaned Files_2020/NonBatModels_IS.csv", row.names = F)
+
 
---
+++ R/01c_Fresnel Comparison.R
@@ -0,0 +1,153 @@
+
+# 01b_Fresnel Comparison ####
+
+Fresnel2021 <- 
+  "Cleaned Files" %>% 
+  list.files(full.names = T) %>% 
+  map(read.csv)
+
+Fresnel2020 <- 
+  "Cleaned Files_2020" %>% 
+  list.files(full.names = T) %>% 
+  map(read.csv)
+
+Fresnel2021 %>% map_dbl(nrow) %>% cbind(
+Fresnel2020 %>% map_dbl(nrow))
+
+FullFresnel <- 
+1:length(Fresnel2021) %>% 
+  map(~full_join(Fresnel2021[[.x]], Fresnel2020[[.x]], 
+                 by = "Sp", 
+                 suffix = c(".2021", ".2020")))
+
+names(FullFresnel) <- c("Bats_In", "Bats_Out", "Mammals_In", "Mammals_Out")
+
+FullFresnel$Bats_In$Betacov.2021 %>% sum(na.rm = T)
+FullFresnel$Bats_In$Betacov.2020 %>% sum(na.rm = T)
+
+# Correlation figures ####
+
+library(tidyverse); library(cowplot); library(colorspace); library(patchwork); library(ggpubr); library(ggregplot)
+library(ggtext)
+
+dir_create("Figures")
+
+theme_set(theme_cowplot() + theme(strip.background = element_rect(fill = "white")))
+
+AlberPalettes <- c("YlGnBu","Reds","BuPu", "PiYG")
+
+AlberColours <- sapply(AlberPalettes, function(a) RColorBrewer::brewer.pal(5, a)[4])
+
+AlberColours[length(AlberColours)+1:2] <- 
+  
+  RColorBrewer::brewer.pal(11, AlberPalettes[[4]])[c(2,10)]
+
+AlberColours <- c(AlberColours, Pink = "#FD6396", Blue = "#3C78D8")
+
+Relabel <- c(glue::glue("Network.{1:4}"),
+             glue::glue("Hybrid.{1}"), 
+             glue::glue("Trait.{1:3}"))
+
+names(Relabel) <- c("R.Po2", "R.Po3",
+                    "R.Dal1","R.Far1",
+                    "R.Stock1",
+                    "R.Gut1", "R.Car3",
+                    "R.Alb")
+
+Relabel[intersect(names(Relabel), names(BatModels_IS))] ->
+  
+  Relabel
+
+# Figure 1: Observed v Predicted panels ####
+
+Fresnel2021[[1]] %>% 
+  #gather("Key", "Value", -c(Sp, Betacov, Rank, PropRank, InSample)) %>%
+  gather("Key", "Value", starts_with("R.")) %>%
+  mutate_at("Key", ~.x %>% recode(!!!Relabel) %>% factor(levels = Relabel)) %>%
+  ggplot(aes(Value, Betacov)) + 
+  geom_point(alpha = 0.3, colour = AlberColours[[3]]) + 
+  geom_smooth(method = glm, 
+              method.args = list(family = "binomial"),
+              fill = NA, colour = "black") +
+  #geom_smooth(method = lm, fill = NA, colour = "black") +
+  #geom_smooth(fill = NA, colour = "black") +
+  facet_wrap(~Key, nrow = 2) +
+  stat_cor(label.y = 1.2,
+           aes(label = ..rr.label..)) +
+  #coord_fixed() +
+  scale_x_continuous(breaks = c(0, 0.5, 1.3)) +
+  scale_y_continuous(breaks = c(0:5/5), limits = c(-0, 1.25)) +
+  labs(x = "Proportional rank") -> 
+  
+  SingleCorrelations_2021
+
+Fresnel2021[[1]] %>% 
+  mutate(Key = "Multi-model ensemble") %>%
+  ggplot(aes(PropRank, Betacov)) + 
+  #ggtitle("Model assemblage") +
+  geom_point(alpha = 0.6, colour = AlberColours[[3]], 
+             position = position_jitter(h = 0.05)) + 
+  #geom_smooth(method = lm, fill = NA, colour = "black") +
+  geom_smooth(method = glm, 
+              method.args = list(family = "binomial"),
+              fill = NA, colour = "black") +
+  # geom_smooth(fill = NA, colour = "black") +
+  #coord_fixed() + 
+  lims(x = c(0, 1)) +
+  scale_y_continuous(breaks = c(0:5/5), 
+                     limits = c(-0.1, 1.25)) +
+  stat_cor(label.y = 1.2, method = "spearman",
+           aes(label = ..rr.label..)) +
+  facet_wrap(~Key) +
+  labs(x = "Proportional rank") ->
+  
+  OverallCorrelations_2021
+
+Fresnel2020[[1]] %>% 
+  #gather("Key", "Value", -c(Sp, Betacov, Rank, PropRank, InSample)) %>%
+  gather("Key", "Value", starts_with("R.")) %>%
+  mutate_at("Key", ~.x %>% recode(!!!Relabel) %>% factor(levels = Relabel)) %>%
+  ggplot(aes(Value, Betacov)) + 
+  geom_point(alpha = 0.3, colour = AlberColours[[3]]) + 
+  geom_smooth(method = glm, 
+              method.args = list(family = "binomial"),
+              fill = NA, colour = "black") +
+  #geom_smooth(method = lm, fill = NA, colour = "black") +
+  #geom_smooth(fill = NA, colour = "black") +
+  facet_wrap(~Key, nrow = 2) +
+  stat_cor(label.y = 1.2,
+           aes(label = ..rr.label..)) +
+  #coord_fixed() +
+  scale_x_continuous(breaks = c(0, 0.5, 1.3)) +
+  scale_y_continuous(breaks = c(0:5/5), limits = c(-0, 1.25)) +
+  labs(x = "Proportional rank") -> 
+  
+  SingleCorrelations_2020
+
+Fresnel2020[[1]] %>% 
+  mutate(Key = "Multi-model ensemble") %>%
+  ggplot(aes(PropRank, Betacov)) + 
+  #ggtitle("Model assemblage") +
+  geom_point(alpha = 0.6, colour = AlberColours[[3]], 
+             position = position_jitter(h = 0.05)) + 
+  #geom_smooth(method = lm, fill = NA, colour = "black") +
+  geom_smooth(method = glm, 
+              method.args = list(family = "binomial"),
+              fill = NA, colour = "black") +
+  # geom_smooth(fill = NA, colour = "black") +
+  #coord_fixed() + 
+  lims(x = c(0, 1)) +
+  scale_y_continuous(breaks = c(0:5/5), 
+                     limits = c(-0.1, 1.25)) +
+  stat_cor(label.y = 1.2, method = "spearman",
+           aes(label = ..rr.label..)) +
+  facet_wrap(~Key) +
+  labs(x = "Proportional rank") ->
+  
+  OverallCorrelations_2020
+
+(SingleCorrelations_2020|OverallCorrelations_2020)/
+  (SingleCorrelations_2021|OverallCorrelations_2021) + 
+  plot_layout(widths = c(1.35,1)) +
+  ggsave("Figures/Obs_Pred_CorrelationsHorizontal.jpeg", 
+         units = "mm", width = 325, height = 150)
